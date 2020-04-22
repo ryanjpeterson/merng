ab81d46b06898096f4b1ad28d22965348b9a248a
Ryan Peterson <ryanpeterson@laptop-fm1f2ovp.gateway.2wire.net>
2020-04-22 09:56:51 -0400

    merng client lesson 4 finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+// Components
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
